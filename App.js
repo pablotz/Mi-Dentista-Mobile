f2672cc066189bcbf80400bde687aec49f0b2466
PabloTz <pablotroll789@hotmail.com>
2021-10-30 19:14:52 -0500

    all ready

--- App.js
+++ App.js
@@ -4,13 +4,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {Text, View} from 'react-native'
 
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
 
 const Stack = createNativeStackNavigator();
 
@@ -18,9 +11,9 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      {/* <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      </Stack.Navigator> */}
     </NavigationContainer>
   )
 }
